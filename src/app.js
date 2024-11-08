c4d497ae1b2470f6d1a737a6c4517f89994d066d
tamilvanan <tamilvanansambasivam@gmail.com>
2024-11-08 18:37:27 +0530

    feat: add http methods

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send("users from get request");
+});
+
+app.post("/users", (req, res) => {
+  res.send("users successfully posted");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("users successfully deleted");
+});
+
+app.patch("/users", (req, res) => {
+  res.send("user updated successfully");
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
